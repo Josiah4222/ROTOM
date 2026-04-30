7fb936ad20f5949578aafbcf2d07c973f0a5d164
Josiah4222 <eyosialex12@gmail.com>
2026-04-30 22:16:46 +0300

    fix SCRIPT_NAME injection in wsgi.py

--- REACHONEETH/wsgi.py
+++ REACHONEETH/wsgi.py
@@ -14,3 +14,15 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'REACHONEETH.settings')
 
 application = get_wsgi_application()
+
+# Wrap application to inject SCRIPT_NAME into WSGI environ
+_application = application
+
+def application(environ, start_response):
+    script_name = os.environ.get('SCRIPT_NAME', '')
+    if script_name:
+        environ['SCRIPT_NAME'] = script_name
+        path_info = environ.get('PATH_INFO', '')
+        if path_info.startswith(script_name):
+            environ['PATH_INFO'] = path_info[len(script_name):]
+    return _application(environ, start_response)
